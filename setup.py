7d34a8a544de701174cf56b492d2af0a52ec466d
Vinay-Kamma <kammavinaybabu@gmail.com>
2025-04-08 22:08:14 +0530

    Project stucture is added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AAI project',
+    version='0.0.0',
+    author='Vinay',
+    author_email='kammavinay6666@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
